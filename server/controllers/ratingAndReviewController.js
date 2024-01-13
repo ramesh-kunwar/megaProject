aab1ea117e4739e5ae5f25c2a3ad87a3a565fcce
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2024-01-13 15:50:37 +0545

    Payment integration backend

---
+++ server/controllers/ratingAndReviewController.js
@@ -0,0 +1,22 @@
+const RatingAndReview = require("../models/RatingAndReviewSchema");
+const Course = require("../models/CourseSchema");
+const User = require("../models/UserSchema");
+
+// @desc    Create a rating and review
+
+exports.createRating = async (req, res) => {
+  try {
+    // get user id
+    // fetch data from the request body
+    // validation
+    // check if user is enrolled in the course or not
+    // check if user has already given rating and review or not
+    // create rating and review
+    // update course
+    // return response
+  } catch (error) {}
+};
+
+// @decs getAverageRating
+
+// @desc    Get all rating and reviews
